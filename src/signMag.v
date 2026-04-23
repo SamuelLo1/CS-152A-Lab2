d8e9ef272175589b71987644b020e6765c02541a
SamuelLo <samuello300@gmail.com>
2026-04-23 13:22:06 -0700

    add: two's complement to sign magnitude and also testbench

---
+++ src/signMag.v
@@ -0,0 +1,49 @@
+/*
+Input: takes in two's complement 12 bit number
+Output: 
+    1 sign bit 
+    11 magnitude bits
+
+Description: 
+    If the sign bit is 0, the magnitude bits are the same as the input bits.
+    If the sign bit is 1, the magnitude bits are the two's complement of the input bits.
+
+    Edge case: In the event of the most negative number possible: -2048 should be reprsented as 1 2048
+*/
+module signMag(
+    input [11:0] in,
+    output S,
+    output [10:0] magnitude
+); 
+
+    reg signBit;
+    reg [10:0] magBits;
+
+    /* 
+    Whenver input changes perform the following
+        - extract the sign bit from the rest of 
+        - take two's complement 
+        - need to ensure edgecase handled 
+
+    */
+    always @(in) begin
+        signBit = in[11];
+        magBits = in[10:0];
+
+        if (signBit == 1'b1) begin
+            /* undo two' comp */ 
+            magBits = ~(in[10:0] - 11'd1);
+        end
+
+        /* edge case */
+        if (in == 12'b100000000000) begin
+            magBits = 11'b11111111111; 
+        end
+
+    end
+
+    assign S = signBit;
+    assign magnitude = magBits;
+
+endmodule
+
